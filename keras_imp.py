5362fbc8e9f1dca0c5b919ddd29bc412f2b74975
Zachar Adiniaev <zahara@art-sim.com>
2019-09-21 20:38:20 +0300

    move to ubuntu

--- keras_imp.py
+++ keras_imp.py
@@ -1,64 +1,51 @@
 # import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import patches
+import os
 
-im = '/homes/zahara/PycharmProjects/text_button_detection/data/file.png'
-image = plt.imread(im)
-plt.imshow(image)
-print('image printed')
+data_dir = 'data/train'
+objects_list = []
+annotate_file = open('annotate.txt', 'w')
 
-# fig = plt.figure()
-#
-# # add axes to the image
-# ax = fig.add_axes([0, 0, 1, 1])
-#
-# # read and plot the image
-# image = plt.imread(im)
-# plt.imshow(image)
-#
-# xmin = 11
-# xmax = 496
-# ymin = 174
-# ymax = 517
-#
-# width = xmax - xmin
-# height = ymax - ymin
-#
-# # assign different color to different classes of objects
-# edgecolor = 'r'
-# ax.annotate('button', xy=(xmax - 40, ymin + 20))
-#
-# # add bounding boxes to the image
-# rect = patches.Rectangle((xmin, ymin), width, height, edgecolor=edgecolor, facecolor='none')
-#
-# ax.add_patch(rect)
-#
-# plt.imshow(image)
-'''
-# iterating over the image for different objects
-for _,row in train[train.image_names == "1.jpg"].iterrows():
-    xmin = row.xmin
-    xmax = row.xmax
-    ymin = row.ymin
-    ymax = row.ymax
-    
-    width = xmax - xmin
-    height = ymax - ymin
-    
-    # assign different color to different classes of objects
-    if row.cell_type == 'RBC':
-        edgecolor = 'r'
-        ax.annotate('RBC', xy=(xmax-40,ymin+20))
-    elif row.cell_type == 'WBC':
-        edgecolor = 'b'
-        ax.annotate('WBC', xy=(xmax-40,ymin+20))
-    elif row.cell_type == 'Platelets':
-        edgecolor = 'g'
-        ax.annotate('Platelets', xy=(xmax-40,ymin+20))
-        
-    # add bounding boxes to the image
-    rect = patches.Rectangle((xmin,ymin), width, height, edgecolor = edgecolor, facecolor = 'none')
-    
-    ax.add_patch(rect)
+for r, d, files in os.walk(data_dir):
+    for i,f in enumerate(files):
+        if '.xml' in f:
+            file_path = os.path.join(data_dir, f)
+            xml_file = open(file_path)
 
-'''
+            image_file = file_path[:-3] + 'png'
+            image = plt.imread(image_file)
+            fig = plt.figure()
+
+            for row in xml_file:
+                if 'xmin' in row:
+                    xmin = int(row.split('>')[1].split('<')[0])
+                if 'ymin' in row:
+                    ymin = int(row.split('>')[1].split('<')[0])
+                if 'xmax' in row:
+                    xmax = int(row.split('>')[1].split('<')[0])
+                if 'ymax' in row:
+                    ymax = int(row.split('>')[1].split('<')[0])
+
+                    # add axes to the image
+                    ax = fig.add_axes([0, 0, 1, 1])
+                    width = xmax - xmin
+                    height = ymax - ymin
+
+                    # assign different color to different classes of objects
+                    edgecolor = 'r'
+                    ax.annotate('', xy=(xmax - 40, ymin + 20))
+
+                    # add bounding boxes to the image
+                    rect = patches.Rectangle((xmin, ymin), width, height, edgecolor=edgecolor, facecolor='none')
+
+                    ax.add_patch(rect)
+
+                    ann_object = f'{os.path.abspath(image_file)},{xmin},{ymin},{xmax},{ymax},button\n'
+                    objects_list.append(ann_object)
+                    annotate_file.write(ann_object)
+
+            # plt.imshow(image)
+            # plt.show()
+
+
